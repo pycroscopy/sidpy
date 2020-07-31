0b3eadee2a0ea99eaaa8c0c35d93e54245080cfb
ssomnath <14300780+ssomnath@users.noreply.github.com>
2020-07-31 18:58:31 -0400

    Importing new submodules correctly

--- sidpy/__init__.py
+++ sidpy/__init__.py
@@ -1,5 +1,5 @@
 """
-The pyUSID package.
+The sidpy package
 
 Submodules
 ----------
@@ -8,10 +8,12 @@ Submodules
     :toctree: _autosummary
 
 """
-from . import io, processing, viz
+from . import base, io, processing, sid, viz
 from .__version__ import version as __version__
 
 __all__ = ['__version__']
+__all__ += base.__all__
 __all__ += io.__all__
 __all__ += processing.__all__
+__all__ += sid.__all__
 __all__ += viz.__all__
