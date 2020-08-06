eda83aa10abee7d19f0132b260d471ac9e93f652
ssomnath <14300780+ssomnath@users.noreply.github.com>
2020-08-06 08:40:07 -0400

    Added hdf to init

--- sidpy/__init__.py
+++ sidpy/__init__.py
@@ -8,11 +8,12 @@ Submodules
     :toctree: _autosummary
 
 """
-from . import base, io, proc, sid, viz
+from . import base, hdf, io, proc, sid, viz
 from .__version__ import version as __version__
 
 __all__ = ['__version__']
 __all__ += base.__all__
+__all__ += hdf.__all__
 __all__ += io.__all__
 __all__ += proc.__all__
 __all__ += sid.__all__
