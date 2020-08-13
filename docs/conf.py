170faa994cc5aea690bbc503ab480bed3c6956f5
ssomnath <14300780+ssomnath@users.noreply.github.com>
2020-08-13 18:12:54 -0400

    Adding recommended lines for napolean

--- docs/conf.py
+++ docs/conf.py
@@ -102,6 +102,21 @@ pygments_style = 'sphinx'
 # If true, keep warnings as "system message" paragraphs in the built documents.
 #keep_warnings = False
 
+# Napoleon settings
+# https://www.sphinx-doc.org/en/master/usage/extensions/napoleon.html
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_type_aliases = None
+
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
 
