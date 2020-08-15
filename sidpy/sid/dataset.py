1f711532bc3b28690a952bcd91950c4fac5f7f2d
ssomnath <14300780+ssomnath@users.noreply.github.com>
2020-08-15 16:38:31 -0400

    PEP8 cleaning Python 2.7 backwards compatibility

--- sidpy/sid/dataset.py
+++ sidpy/sid/dataset.py
@@ -1,22 +1,36 @@
-# starting code from
-# https://scikit-allel.readthedocs.io/en/v0.21.1/_modules/allel/model/dask.html
+# -*- coding: utf-8 -*-
+"""
+Abstract :class:`~sidpy.io.dataset.Dataset` base-class
 
+Created on Tue Nov  3 15:07:16 2015
+
+@author: Gerd Duscher
+
+starting code from:
+https://scikit-allel.readthedocs.io/en/v0.21.1/_modules/allel/model/dask.html
+"""
+
+from __future__ import division, print_function, absolute_import, unicode_literals
 import numpy as np
 import string
-
 import dask.array as da
-
 from .dimension import Dimension
-
-from warnings import warn
-
-#from .hdf_utils import check_if_main, get_attr, create_results_group, link_as_main, write_main_dataset, copy_attributes
 
 
 def get_chunks(data, chunks=None):
-    """Try to guess a reasonable chunk shape to use for block-wise
-    algorithms operating over `data`."""
+    """
+    Try to guess a reasonable chunk shape to use for block-wise
+    algorithms operating over `data`.
 
+    Parameters
+    ----------
+    data
+    chunks
+
+    Returns
+    -------
+
+    """
     if chunks is None:
 
         if hasattr(data, 'chunklen') and hasattr(data, 'shape'):
@@ -44,6 +58,16 @@ def get_chunks(data, chunks=None):
 
 
 def ensure_array_like(data):
+    """
+
+    Parameters
+    ----------
+    data
+
+    Returns
+    -------
+
+    """
     if not hasattr(data, 'shape') or not hasattr(data, 'dtype'):
         a = np.asarray(data)
         if len(a.shape) == 0:
@@ -54,6 +78,17 @@ def ensure_array_like(data):
 
 
 def ensure_dask_array(data, chunks=None):
+    """
+
+    Parameters
+    ----------
+    data
+    chunks
+
+    Returns
+    -------
+
+    """
     if isinstance(data, da.Array):
         if chunks:
             data = data.rechunk(chunks)
@@ -65,7 +100,18 @@ def ensure_dask_array(data, chunks=None):
 
 
 def view_subclass(darr, cls):
-    """View a dask Array as an instance of a dask Array sub-class."""
+    """
+    View a dask Array as an instance of a dask Array sub-class.
+
+    Parameters
+    ----------
+    darr
+    cls
+
+    Returns
+    -------
+
+    """
     return cls(darr.dask, name=darr.name, chunks=darr.chunks,
                dtype=darr.dtype, shape=darr.shape)
 
@@ -96,11 +142,23 @@ class Dataset(da.Array):
     """
 
     def __init__(self, *args, **kwargs):
-        super(Dataset, self).__init__()
-
+        super(Dataset, self).__init__(*args, **kwargs)
 
     @classmethod
     def from_array(cls, x, chunks=None, name=None, lock=False):
+        """
+
+        Parameters
+        ----------
+        x
+        chunks
+        name
+        lock
+
+        Returns
+        -------
+
+        """
         # override this as a class method to allow sub-classes to return
         # instances of themselves
 
@@ -139,9 +197,20 @@ class Dataset(da.Array):
         return cls
 
     def like_data(self, data,  name=None, lock=False):
+        """
+
+        Parameters
+        ----------
+        data
+        name
+        lock
+
+        Returns
+        -------
+
+        """
 
         new_data = self.from_array(data, chunks=None, name=None, lock=False)
-
 
         new_data.data_type = self.data_type
         new_data.units = self.units
@@ -152,14 +221,14 @@ class Dataset(da.Array):
         new_data.source = self.source
         new_data.data_descriptor = ''
 
-
         for dim in range(new_data.ndim):
             # TODO: add parent to dimension to set attribute if name changes
             new_data.labels.append(string.ascii_lowercase[dim])
             if len(self.axes[dim].values) ==  new_data.shape[dim]:
                 new_data.set_dimension(dim,self.axes[dim])
             else:
-                ## asumes the axis scale is equidistant -Suhas has a function somewhere to test that
+                # assumes the axis scale is equidistant
+                # Suhas has a function somewhere to test that
                 # TODO: test below
                 scale = self.axes[dim].values[1] - self.axes[dim].values[1]
                 axis = self.axes[dim].copy()
@@ -271,7 +340,13 @@ class Dataset(da.Array):
                 annotations_group.attrs[key] = item
     """
     def copy(self):
-        dset_copy = NSIDask.from_array(self, self.chunks, self.name)
+        """
+
+        Returns
+        -------
+
+        """
+        dset_copy = Dataset.from_array(self, self.chunks, self.name)
 
         dset_copy.title = self.title
         dset_copy.units = self.units
@@ -288,7 +363,18 @@ class Dataset(da.Array):
         return dset_copy
 
     def set_dimension(self,dim, dimension):
-        #TODO: Check whether dimension valid
+        """
+
+        Parameters
+        ----------
+        dim
+        dimension
+
+        Returns
+        -------
+
+        """
+        # TODO: Check whether dimension valid
         setattr(self, dimension.name,dimension)
         setattr(self, 'dim_{}'.format(dim), dimension)
 
@@ -296,20 +382,29 @@ class Dataset(da.Array):
 
     def get_extent(self, dimensions):
         """
-        get image extend as neeed i.e. in matplotlib's imshow function. This function works for equi or non-equi spaced axes
+        get image extend as neeed i.e. in matplotlib's imshow function.
+        This function works for equi or non-equi spaced axes
+
+        Parameters
+        ----------
+        dimensions
+
+        Returns
+        -------
+
         """
         extend = []
         for i, dim in enumerate(dimensions):
-            start = self.axes[dim].values[0] - (self.axes[dim].values[1] -self.axes[dim].values[0])/2
-            end = self.axes[dim].values[-1]  - (self.axes[dim].values[-1]-self.axes[dim].values[-2])/2
+            temp = self.axes[dim].values
+            start = temp[0] - (temp[1] - temp[0])/2
+            end = temp[-1] - (temp[-1] - temp[-2])/2
             if i == 1:
-                extend.append(end) #y axis starts on top
+                extend.append(end)  # y axis starts on top
                 extend.append(start)
             else:
                 extend.append(start)
                 extend.append(end)
         return extend
-
 
     @property
     def labels(self):
@@ -321,6 +416,7 @@ class Dataset(da.Array):
     @property
     def title(self):
         return self._title
+
     @title.setter
     def title(self, value):
         if isinstance(value, str):
@@ -331,6 +427,7 @@ class Dataset(da.Array):
     @property
     def units(self):
         return self._units
+
     @units.setter
     def units(self, value):
         if isinstance(value, str) :
@@ -338,10 +435,10 @@ class Dataset(da.Array):
         else:
             raise ValueError('units needs to be a string')
 
-
     @property
     def data_descriptor(self):
         return self._data_descriptor
+
     @data_descriptor.setter
     def data_descriptor(self, value):
         if isinstance(value, str):
@@ -352,6 +449,7 @@ class Dataset(da.Array):
     @property
     def data_type(self):
         return self._data_type
+
     @data_type.setter
     def data_type(self, value):
         if isinstance(value, str):
