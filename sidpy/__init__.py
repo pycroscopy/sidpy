344cc0036721bcb652ae4b4b91537e4395d772bc
ssomnath <14300780+ssomnath@users.noreply.github.com>
2020-08-15 16:26:42 -0400

    Enabling import of individual modules directly

--- sidpy/__init__.py
+++ sidpy/__init__.py
@@ -8,14 +8,31 @@ Submodules
     :toctree: _autosummary
 
 """
-from . import base, hdf, io, proc, sid, viz
 from .__version__ import version as __version__
+from . import base, hdf, io, proc, sid, viz
+from .base import dict_utils, num_utils, string_utils
+from .hdf import hdf_utils, reg_ref, dtype_utils
+from .io import interface_utils
+from .proc import comp_utils
+from .sid.dimension import Dimension
+from .sid.translator import Translator
+from .sid.dataset import Dataset
+from .viz import plot_utils
+from .viz import jupyter_utils
 
 __all__ = ['__version__']
+# Traditional hierarchical approach - importing submodules
 __all__ += base.__all__
 __all__ += hdf.__all__
 __all__ += io.__all__
 __all__ += proc.__all__
 __all__ += sid.__all__
 __all__ += viz.__all__
-from .sid.dataset import Dataset
+
+# Making things easier by surfacing all low-level modules directly:
+__all__ += ['dict_utils', 'num_utils', 'string_utils']
+__all__ += ['hdf_utils', 'reg_ref', 'dtype_utils']
+__all__ += ['interface_utils']
+__all__ += ['comp_utils']
+__all__ += ['Dimension', 'Translator', 'Dataset']
+__all__ += ['plot_utils', 'jupyter_utils']
