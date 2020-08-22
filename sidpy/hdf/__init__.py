b8540c0cdbb6e38ae895c9b6c2f0d641a7f848fd
ssomnath <14300780+ssomnath@users.noreply.github.com>
2020-08-22 16:49:39 -0400

    Added prov_utils to init

--- sidpy/hdf/__init__.py
+++ sidpy/hdf/__init__.py
@@ -10,7 +10,8 @@ Submodules
     hdf_utils
     dtype_utils
     reg_ref
+    prov_utils
 """
-from . import hdf_utils, reg_ref, dtype_utils
+from . import hdf_utils, prov_utils, reg_ref, dtype_utils
 
-__all__ = ['hdf_utils', 'reg_ref', 'dtype_utils']
+__all__ = ['hdf_utils', 'prov_utils', 'reg_ref', 'dtype_utils']
