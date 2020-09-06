4a4ea9b6dde9c59180867d503f515d8bafc43765
ssomnath <14300780+ssomnath@users.noreply.github.com>
2020-09-06 16:02:53 -0400

    Corrected authors

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ setup(
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     url='https://pycroscopy.github.io/sidpy/about.html',
     license='MIT',
-    author='Suhas Somnath and contributors',
+    author='Suhas Somnath, Gerd Duscher, and contributors',
     author_email='pycroscopy@gmail.com',
     install_requires=requirements,
     setup_requires=['pytest-runner'],
