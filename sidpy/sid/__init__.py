6352ed69811227cebe14e1a22f128a1cc3ef1bb7
ssomnath <14300780+ssomnath@users.noreply.github.com>
2020-09-18 16:36:41 -0400

    Importing Enums

--- sidpy/sid/__init__.py
+++ sidpy/sid/__init__.py
@@ -2,9 +2,10 @@
 Spectroscopy and Imaging Data related classes
 """
 
-from .dimension import Dimension
+from .dimension import Dimension, DimensionTypes
 from .translator import Translator
-from .dataset import Dataset
+from .dataset import Dataset, DataTypes
 from .reader import Reader
 
-__all__ = ['Dimension', 'Translator', 'Dataset', 'Reader']
+__all__ = ['Dimension', 'DimensionTypes', 'Dataset', 'DataTypes', 'Reader',
+           'Translator']
