4d2f48376f606fe84364a7d586ce6e2a6dc6b011
ssomnath <14300780+ssomnath@users.noreply.github.com>
2020-09-25 17:01:19 -0400

    Accounting for 1 core machines PEP8 cleaning

--- tests/proc/test_comp_utils.py
+++ tests/proc/test_comp_utils.py
@@ -23,34 +23,50 @@ class TestRecommendCPUCores(unittest.TestCase):
         num_jobs = 14035
         if MAX_CPU_CORES > 4:
             min_free_cores = 2
+        elif MAX_CPU_CORES == 1:
+            min_free_cores = 0
         else:
             min_free_cores = 1
-        ret_val = comp_utils.recommend_cpu_cores(num_jobs, lengthy_computation=False)
+        ret_val = comp_utils.recommend_cpu_cores(num_jobs,
+                                                 lengthy_computation=False)
         self.assertEqual(ret_val, max(1, MAX_CPU_CORES-min_free_cores))
-        ret_val = comp_utils.recommend_cpu_cores(num_jobs, requested_cores=1, lengthy_computation=False)
+        ret_val = comp_utils.recommend_cpu_cores(num_jobs,
+                                                 requested_cores=1,
+                                                 lengthy_computation=False)
         self.assertEqual(ret_val, 1)
-        ret_val = comp_utils.recommend_cpu_cores(num_jobs, requested_cores=MAX_CPU_CORES, lengthy_computation=False)
+        ret_val = comp_utils.recommend_cpu_cores(num_jobs,
+                                                 requested_cores=MAX_CPU_CORES,
+                                                 lengthy_computation=False)
         self.assertEqual(ret_val, MAX_CPU_CORES)
-        ret_val = comp_utils.recommend_cpu_cores(num_jobs, requested_cores=5000, lengthy_computation=False)
+        ret_val = comp_utils.recommend_cpu_cores(num_jobs,
+                                                 requested_cores=5000,
+                                                 lengthy_computation=False)
         self.assertEqual(ret_val, MAX_CPU_CORES)
 
     def test_changing_min_cores(self):
         num_jobs = 14035
         for min_free_cores in range(1, MAX_CPU_CORES):
-            ret_val = comp_utils.recommend_cpu_cores(num_jobs, lengthy_computation=False, min_free_cores=min_free_cores)
+            ret_val = comp_utils.recommend_cpu_cores(num_jobs,
+                                                     lengthy_computation=False,
+                                                     min_free_cores=min_free_cores)
             self.assertEqual(ret_val, max(1, MAX_CPU_CORES - min_free_cores))
 
     def test_illegal_min_free_cores(self):
         num_jobs = 14035
         min_free_cores = MAX_CPU_CORES
         with self.assertRaises(ValueError):
-            _ = comp_utils.recommend_cpu_cores(num_jobs, lengthy_computation=False, min_free_cores=min_free_cores)
+            _ = comp_utils.recommend_cpu_cores(num_jobs,
+                                               lengthy_computation=False,
+                                               min_free_cores=min_free_cores)
 
     def test_few_small_jobs(self):
         num_jobs = 13
-        ret_val = comp_utils.recommend_cpu_cores(num_jobs, lengthy_computation=False)
+        ret_val = comp_utils.recommend_cpu_cores(num_jobs,
+                                                 lengthy_computation=False)
         self.assertEqual(ret_val, 1)
-        ret_val = comp_utils.recommend_cpu_cores(num_jobs, requested_cores=MAX_CPU_CORES, lengthy_computation=False)
+        ret_val = comp_utils.recommend_cpu_cores(num_jobs,
+                                                 requested_cores=MAX_CPU_CORES,
+                                                 lengthy_computation=False)
         self.assertEqual(ret_val, 1)
 
     def test_few_large_jobs(self):
@@ -59,9 +75,12 @@ class TestRecommendCPUCores(unittest.TestCase):
             min_free_cores = 2
         else:
             min_free_cores = 1
-        ret_val = comp_utils.recommend_cpu_cores(num_jobs, lengthy_computation=True)
+        ret_val = comp_utils.recommend_cpu_cores(num_jobs,
+                                                 lengthy_computation=True)
         self.assertEqual(ret_val, max(1, MAX_CPU_CORES-min_free_cores))
-        ret_val = comp_utils.recommend_cpu_cores(num_jobs, requested_cores=MAX_CPU_CORES - 1, lengthy_computation=True)
+        ret_val = comp_utils.recommend_cpu_cores(num_jobs,
+                                                 requested_cores=max(1, MAX_CPU_CORES - 1),
+                                                 lengthy_computation=True)
         self.assertEqual(ret_val, max(1, MAX_CPU_CORES - 1))
 
     def test_invalid_min_cores(self):
