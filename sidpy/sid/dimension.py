c0f516c225ac1f0be2b7917633cb583141eb3393
Raj <rgiri@uw.edu>
2020-10-28 18:50:03 -0700

    Less strict values length check, allows 1-length measurements

--- sidpy/sid/dimension.py
+++ sidpy/sid/dimension.py
@@ -74,10 +74,10 @@ class Dimension(np.ndarray):
         """
 
         if isinstance(values, int):
-            if values < 2:
+            if values < 1:
                 raise TypeError("values should at least be specified as a positive integer")
             values = np.arange(values)
-        elif len(np.array(values)) < 2:
+        elif len(np.array(values)) < 1:
             raise TypeError("values should at least be specified as a positive integer")
         if np.array(values).ndim != 1:
             raise ValueError('Dimension can only be 1 dimensional')
