d9148e40b44a995ad2925c9ced87833a885d2f36
ssomnath <14300780+ssomnath@users.noreply.github.com>
2020-10-30 16:27:29 -0400

    Fixed tests for get source dataset

--- tests/hdf/test_prov_utils.py
+++ tests/hdf/test_prov_utils.py
@@ -421,24 +421,24 @@ class TestCheckForOld(TestHDFUtilsBase):
             self.assertEqual(len(ret_val), 0)
 
 
-class TestGetSourceDataset(TestSimple):
+class TestGetSourceDataset(TestHDFUtilsBase):
 
     def test_legal(self):
         with h5py.File(data_utils.std_beps_path, mode='r') as h5_f:
             h5_groups = [h5_f['/Raw_Measurement/source_main-Fitter_000'],
                         h5_f['/Raw_Measurement/source_main-Fitter_001']]
-            h5_main = USIDataset(h5_f['/Raw_Measurement/source_main'])
+            h5_main = h5_f['/Raw_Measurement/source_main']
             for h5_grp in h5_groups:
-                self.assertEqual(h5_main, hdf_utils.get_source_dataset(h5_grp))
+                self.assertEqual(h5_main, prov_utils.get_source_dataset(h5_grp))
 
     def test_invalid_type(self):
         with self.assertRaises(TypeError):
-            _ = hdf_utils.get_source_dataset('/Raw_Measurement/Misc')
+            _ = prov_utils.get_source_dataset('/Raw_Measurement/Misc')
 
     def test_illegal(self):
         with h5py.File(data_utils.std_beps_path, mode='r') as h5_f:
             with self.assertRaises(ValueError):
-                _ = hdf_utils.get_source_dataset(h5_f['/Raw_Measurement/Misc'])
+                _ = prov_utils.get_source_dataset(h5_f['/Raw_Measurement/Misc'])
 
 
 if __name__ == '__main__':
