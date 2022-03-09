62a77140ae030c48c15ed4697467fb63ee44124d
Mani <valletisai.mani@gmail.com>
2022-03-09 15:21:14 -0500

    Docstring of fitter class is updated and a new parameter 'return_fit' is added to the class

--- sidpy/proc/process.py
+++ sidpy/proc/process.py
@@ -9,20 +9,19 @@ try:
 except ModuleNotFoundError:
     curve_fit = None
 
-import matplotlib.pyplot as plt
-import inspect
-
 
 class SidFitter:
     # An extension of the Process Class for Functional Fitting
     def __init__(self, sidpy_dataset, fit_fn, xvec=None, ind_dims=None, guess_fn=None, num_fit_parms=None,
-                 return_cov=False, fit_parameter_labels=None, num_workers=2, threads=2):
+                 return_cov=False, return_fit=False, fit_parameter_labels=None, num_workers=2, threads=2):
         """
         Parameters
         ----------
         sidpy_dataset: (sidpy.Dataset) Sidpy dataset object to be fit
 
         fit_fn: (function) Function used for fitting.
+        Should take xvec as the first argument and parameters as the rest of the arguments.
+        Should return the function value at each of the points in the xvec
 
         xvec: (numpy ndarray or list of numpy ndarrays) (Optional)
         Independent variable for fitting. Should be an array
@@ -36,10 +35,14 @@ class SidFitter:
         It takes the same arguments as the fitting function and should return the same type of results array.
         If the guess_fn is NOT provided, then the user MUST input the num_fit_parms.
 
-        num_fit_parms: (int) Number of fitting parameters. This is needed IF the guess function is not provided.
+        num_fit_parms: (int) Number of fitting parameters. This is needed IF the guess function is not provided to set
+        the priors for the parameters for the curve_fit function.
 
         return_cov: (bool) (default False) Returns the estimated covariance of fitting parameters. Confer
         scipy.optimize.curve_fit for further details
+
+        return_fit: (bool) (default False) Returns the fitted sidpy dataset using the optimal parameters
+         when set to true
 
         fit_parameter_labels: (list) (default None) List of parameter labels
 
@@ -47,18 +50,32 @@ class SidFitter:
 
         threads: (int) (default =2) Number of threads to use when setting up Dask client
 
+
+        Returns:
+        -------
+        sidpy.dataset: if return_cov and return_fit are both set to False
+        List: containing sidpy.dataset objects, if either of return_cov or return fit is set to True
+
+        If multiple datasets are expected, the order of the returned datasets is
+
+        [sidpy.dataset with mean parameter values,
+        sidpy.dataset with estimated covariances of the fitting parameters,
+        sidpy.dataset that is fit with the parameters obtained after fitting]
+
         """
+
         if guess_fn is None:
             if num_fit_parms is None:
                 raise ValueError("You did not supply a guess function, you must at least provide number of fit "
-                                 "parameters")
-        self.dataset = sidpy_dataset
-        self.fit_fn = fit_fn
-        self.num_fit_parms = num_fit_parms
+                                 "parameters to set the priors for scipy.optimize.curve_fit")
+        self.dataset = sidpy_dataset  # Sidpy dataset
+        self.fit_fn = fit_fn           # function that takes xvec, *parameters and returns yvec at each value of xvec
+        self.num_fit_parms = num_fit_parms  # int: number of fitting parameters
 
         if ind_dims is not None:
-            self.ind_dims = ind_dims
+            self.ind_dims = ind_dims  # Tuple: containing indices of independent dimensions
         else:
+            # All the dimensions that are not spectral will be considered as independent dimensions
             ind_dims = []
             for i, dim in self.dataset._axes.items():
                 if dim.dimension_type != DimensionType.SPECTRAL:
@@ -70,7 +87,7 @@ class SidFitter:
             raise NotImplementedError('No Spectral (dependent) dimensions found to fit')
 
         # Let's get the dependent dims here
-        dep_dims = []
+        dep_dims = []  # Tuple: contains all the dependent dimensions. ind_dims+dep_dims = all_dims
         for d in np.arange(len(self.dataset.shape)):
             if d not in self.ind_dims:
                 dep_dims.extend([d])
@@ -113,15 +130,17 @@ class SidFitter:
 
         self._setup_calc()
         self.guess_fn = guess_fn
-        self.prior = None
+        self.prior = None   # shape = [num_computations, num_fitting_parms]
         self.fit_labels = fit_parameter_labels
-        self.guess_results = []
-        self.fit_results = []
         self.num_workers = num_workers
         self.threads = threads
         self.guess_completed = False
+        self.return_fit = return_fit
         self.return_cov = return_cov
 
+        self.mean_fit_results = []
+        if self.return_cov:
+            self.cov_fit_results = None
         # set up dask client
         self.client = Client(threads_per_worker=self.threads, n_workers=self.num_workers)
 
@@ -141,32 +160,35 @@ class SidFitter:
                 self.fold_order.append([i])
 
         self.folded_dataset = self.dataset.fold(dim_order=self.fold_order)
+
         # Here is the tricky part, dataset.unfold is designed to get back the original dataset with minimal loss of
-        # information. To do this, unfold utilizes the saved information of the original dataset. Now we are going to
-        # tweak that information and use the unfold function operate after the fitting.
+        # information. To do this, unfold utilizes the saved information while folding the original dataset.
+        # Here, we are going to tweak that information and use the unfold method on the dataset with fitted parameters.
 
         self._unfold_attr = {'dim_order_flattened': self.fold_order[0] + [len(self.fold_order[0])],
                              'shape_transposed': [self.dataset.shape[i] for i in self.fold_order[0]] + [-1]}
         axes = self.dataset._axes.copy()
-        axes.popitem()
+        for i in self.ind_dims:
+            del axes[i]
         self._unfold_attr['_axes'] = axes
-
-        self.fit_results = []
 
     def do_guess(self):
         """
-        Get back to this
-        Returns
+        If a guess_fn is provided: Applies the guess_fn to get priors for the fitting parameters.
+        self.prior is set as the output of guess function at each of the ind_dims
+
+        Returns:
+        None
         -------
 
         """
-        self.guess_results = []
+        guess_results = []
         for ind in range(self.num_computations):
             lazy_result = dask.delayed(self.guess_fn)(self.dep_vec, self.folded_dataset[ind, :])
-            self.guess_results.append(lazy_result)
+            guess_results.append(lazy_result)
 
-        self.guess_results = dask.compute(*self.guess_results)
-        self.prior = np.squeeze(np.array(self.guess_results))
+        guess_results = dask.compute(*guess_results)
+        self.prior = np.squeeze(np.array(guess_results))
         self.num_fit_parms = self.prior.shape[-1]
         self.guess_completed = True
 
@@ -179,8 +201,7 @@ class SidFitter:
             # Calling the guess function
             self.do_guess()
 
-        self.fit_results = []
-
+        fit_results = []
         for ind in range(self.num_computations):
             if self.prior is None:
                 p0 = np.random.normal(loc=0.5, scale=0.1, size=self.num_fit_parms)
@@ -189,28 +210,25 @@ class SidFitter:
 
             lazy_result = dask.delayed(SidFitter.default_curve_fit)(self.fit_fn, self.dep_vec, self.folded_dataset[ind, :],
                                                                     return_cov=self.return_cov, p0=p0, **kwargs)
-            self.fit_results.append(lazy_result)
+            fit_results.append(lazy_result)
 
-        self.fit_results_comp = dask.compute(*self.fit_results)
+        fit_results_comp = dask.compute(*fit_results)
 
         if not self.return_cov:
             # in this case we can just dump it to an array because we only got the parameters back
-            mean_fit_results = np.squeeze(np.array(self.fit_results_comp))
-            cov_results = None
+            self.mean_fit_results = np.squeeze(np.array(fit_results_comp))
 
         else:
             # here we get back both: the parameter means and the covariance matrix!
-            mean_fit_results = np.squeeze(
-                np.array([self.fit_results_comp[ind][0] for ind in range(len(self.fit_results_comp))]))
-            cov_results = np.squeeze(
-                np.array([self.fit_results_comp[ind][1] for ind in range(len(self.fit_results_comp))]))
-            # cov_results_reshaped_shape = self.pos_dim_shapes + cov_results[0].shape
-            # self.cov_results = np.array(cov_results).reshape(cov_results_reshaped_shape)
+            self.mean_fit_results = np.squeeze(
+                np.array([fit_results_comp[ind][0] for ind in range(len(fit_results_comp))]))
+            self.cov_fit_results = np.squeeze(
+                np.array([fit_results_comp[ind][1] for ind in range(len(fit_results_comp))]))
 
         # Here we have either the mean fit results or both mean and cov arrays. We make 2 sidpy dataset out of them
         # Make a sidpy dataset
-        self.mean_fit_results = mean_fit_results
-        mean_sid_dset = Dataset.from_array(mean_fit_results, title='Fitting_Map')
+
+        mean_sid_dset = Dataset.from_array(self.mean_fit_results, title='Fitting_Map')
         mean_sid_dset.metadata['fold_attr'] = self._unfold_attr.copy()
         mean_sid_dset = mean_sid_dset.unfold()
 
@@ -229,14 +247,14 @@ class SidFitter:
         mean_sid_dset.set_dimension(len(mean_sid_dset.shape) - 1, fit_dim)
 
         # Here we deal with the covariance dataset
-        if cov_results is not None:
+        if self.return_cov:
             # Make a sidpy dataset
-            cov_sid_dset = Dataset.from_array(cov_results, title='Fitting_Map_Covariance')
-            num_fit_parms = mean_fit_results.shape[-1]
+            cov_sid_dset = Dataset.from_array(self.cov_fit_results, title='Fitting_Map_Covariance')
             fold_attr = self._unfold_attr.copy()
             fold_attr['dim_order_flattened'] = fold_attr['dim_order_flattened'] + [
                 len(fold_attr['dim_order_flattened'])]
-            fold_attr['shape_transposed'] = fold_attr['shape_transposed'][:-1] + [num_fit_parms] + [num_fit_parms]
+            fold_attr['shape_transposed'] = fold_attr['shape_transposed'][:-1] + [self.num_fit_parms] + \
+                                            [self.num_fit_parms]
 
             cov_sid_dset.metadata['fold_attr'] = fold_attr
             cov_sid_dset = cov_sid_dset.unfold()
@@ -260,8 +278,12 @@ class SidFitter:
             for i, dim in enumerate(cov_dims):
                 cov_sid_dset.set_dimension(i - 2 + len(cov_sid_dset.shape), dim)
 
-        if self.return_cov:
-            return mean_sid_dset, cov_sid_dset
+        if self.return_cov and self.return_fit:
+            return [mean_sid_dset, cov_sid_dset, self.get_fitted_dataset()]
+        elif self.return_cov and not self.return_fit:
+            return [mean_sid_dset, cov_sid_dset]
+        elif not self.return_cov and self.return_fit:
+            return [mean_sid_dset, self.get_fitted_dataset()]
         else:
             return mean_sid_dset
 
