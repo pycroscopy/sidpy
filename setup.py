908be370461c2dfb0e054df1d6a616733b34c096
Mani <valletisai.mani@gmail.com>
2022-04-01 18:28:53 -0400

    Added sklearn as an extra requirement for fitter class. Updated file: setup.py

--- setup.py
+++ setup.py
@@ -66,9 +66,9 @@ setup(
     include_package_data=True,
     # https://setuptools.readthedocs.io/en/latest/setuptools.html#declaring-dependencies
     extras_require={
-        'MPI':  ["mpi4py"],
+        'MPI': ["mpi4py"],
         'File_Widgets': ['pyqt5'],
-        'fitter': ['scipy']
+        'fitter': ['scipy', 'sklearn']
     },
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
