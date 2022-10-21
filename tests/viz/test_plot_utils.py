699087ed560b1c890d41310837981b1802ea4393
Gerd Duscher <50049264+gduscher@users.noreply.github.com>
2022-10-21 17:18:35 -0400

    fixed viz/test_plot_utils

--- tests/viz/test_plot_utils.py
+++ tests/viz/test_plot_utils.py
@@ -515,7 +515,7 @@ class TestPlotScree(unittest.TestCase):
             plot_utils.plot_scree(scree)
 
     def test_scree_h5py_dataset(self):
-        h5_f = h5py.File('test12.h5', 'a')
+        h5_f = h5py.File('test13.h5', 'a')
         scree = h5_f.create_dataset("test", data=np.arange(1,25))
         plot_utils.plot_scree(scree)
        
