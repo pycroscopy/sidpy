ef8098577fd23b24dd504d272df89972f68adb1a
Rama Vasudevan <vasudevanrk@ornl.gov>
2023-05-05 16:07:46 -0400

    complex

--- sidpy/proc/fitter.py
+++ sidpy/proc/fitter.py
@@ -410,7 +410,7 @@ class SidFitter:
         # In that case we need all the spectral dimensions collapsed into a single dimension for kMeans
         # In case of a 1D fit the next line essentially does nothing.
         km_dset = self.folded_dataset.fold(dim_order)
-        if km_dset.dtype == np.complex128:
+        if km_dset.dtype == complex:
             print('Warning: complex dataset detected. Will treat real part only')
             km_dset = np.abs(km_dset)
         
