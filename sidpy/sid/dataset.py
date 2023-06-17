c86aaea0c9973a768ef43d1695ee678fb80e079f
Mani <valletisai.mani@gmail.com>
2023-06-17 11:29:57 -0400

    In set_dimension method, a new attribute '_dataset' is added to the dimension which points to the parent dataset of the dimension. This adds circular dependency where dataset and dimension objects are attributed to each other. I want to see if this breaks tests.

--- sidpy/sid/dataset.py
+++ sidpy/sid/dataset.py
@@ -450,6 +450,7 @@ class Dataset(da.Array):
                              )
 
         dim = dimension.copy()
+        dim._dataset = self
 
         try:
             if hasattr(self, self._axes[ind].name):
