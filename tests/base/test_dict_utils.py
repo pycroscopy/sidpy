b995f7f68173ca4db90146d3c61aed9d86bd4ba7
Gerd Duscher <50049264+gduscher@users.noreply.github.com>
2025-06-19 11:39:49 -0400

    y

--- tests/base/test_dict_utils.py
+++ tests/base/test_dict_utils.py
@@ -8,7 +8,10 @@ from __future__ import division, print_function, unicode_literals, \
     absolute_import
 import unittest
 import sys
-sys.path.append("..//..//")
+sys.path.append("..//..//sidpy")
+import sidoy
+
+print(sidpy.__version__)
 from sidpy.base.dict_utils import *
 
 if sys.version_info.major == 3:
