ddd8ab98e10cc25ef35730a5c4bfb04ff215c4b3
Gerd Duscher <50049264+gduscher@users.noreply.github.com>
2025-06-19 11:51:09 -0400

    Update __init__.py

--- sidpy/proc/__init__.py
+++ sidpy/proc/__init__.py
@@ -1,7 +1,15 @@
 """
 Basic computational utilities
 """
+fitter_present = True
+try:
+    from dask import distributed
+except:
+    fitter_present = False
+if not fitter_present:
+    __all__ = []
+else:
+    from . import fitter
 
-from . import fitter
+    __all__ = ['fitter']
 
-__all__ = ['fitter']
