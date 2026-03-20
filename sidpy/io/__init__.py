6a497a2729eb188f029e44ea113bdb4ce428351c
Rama Vasudevan <vasudevanrk@ornl.gov>
2026-03-20 11:29:25 -0400

    nexus compatibility

--- sidpy/io/__init__.py
+++ sidpy/io/__init__.py
@@ -2,5 +2,9 @@
 User interface utilities
 """
 from . import interface_utils
+from . import nexus
 from .interface_utils  import FileWidget, ChooseDataset
-__all__ = ['interface_utils', 'FileWidget', 'ChooseDataset']
+from .nexus import sidpy_to_nexus_hdf5, nexus_to_sidpy
+
+__all__ = ['interface_utils', 'nexus', 'FileWidget', 'ChooseDataset',
+           'sidpy_to_nexus_hdf5', 'nexus_to_sidpy']
