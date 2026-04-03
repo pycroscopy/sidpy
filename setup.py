05ef3901dea3fc7427c53f28caf72ad3c2165182
ramav87 <rama87@gmail.com>
2026-04-03 10:23:49 -0400

    Add SciFiReaders test dependency

--- setup.py
+++ setup.py
@@ -71,7 +71,8 @@ setup(
     include_package_data=True,
     # https://setuptools.readthedocs.io/en/latest/setuptools.html#declaring-dependencies
     extras_require={
-        'MPI': ["mpi4py"], 'test': ["pytest", "pytest-cov", "SciFiReaders"]
+        'MPI': ["mpi4py"],
+        'test': ["pytest", "pytest-cov", "SciFiReaders", "aicspylibczi"],
     },
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
